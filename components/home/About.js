c6106f53eca92f3524e628b26cb348c273f67634
Chitraksh Maheshwari <68012660+chitrakshbotwala@users.noreply.github.com>
2023-09-14 19:07:38 +0530

    Update About.js

--- components/home/About.js
+++ components/home/About.js
@@ -8,7 +8,7 @@ export default function About() {
     >
       <p className="text-4xl font-bold">About Us</p>
       <p className="text-gray-500 text-xl w-full md:w-8/12 text-center my-4">
-      Welcome to Teenagers for good, a dynamic and passionate non-governmental organization (NGO) powered by the energy, creativity, and dedication of young individuals like us! We are a group of teenagers who believe in the power of youth & want to help the imporvished and unhelped people of India and the world , TFG is a non-profit organization founded and run by a passionate group of teenagers who share a common goal: to alleviate poverty and improve the lives of those in need across India. Our members are driven by a strong sense of social responsibility and a desire to make a meaningful impact
+      Welcome to Teenagers for good, a dynamic and passionate non-profit organization (NPO) powered by the energy, creativity, and dedication of young individuals like us! We are a group of teenagers who believe in the power of youth & want to help the imporvished and unhelped people of India and the world , TFG is a non-profit organization founded and run by a passionate group of teenagers who share a common goal: to alleviate poverty and improve the lives of those in need across India. Our members are driven by a strong sense of social responsibility and a desire to make a meaningful impact
         <br />
         At Teenagers for Good, our mission is simple but profound: to empower teenagers to be active agents of positive change in their communities and beyond. We believe that age should never be a barrier to making a difference, and we are committed to harnessing the unique perspectives, talents, and enthusiasm of teenagers to help the poor and the unhelped.
        <br />
